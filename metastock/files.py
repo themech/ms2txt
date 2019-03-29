a1e2f0907f4cd86a8d399ea1d088ec3c1369e1d9
mech <wozniak.jacek@gmail.com>
2019-03-30 00:10:05 +0100

    Don't decode empty bytes

--- metastock/files.py
+++ metastock/files.py
@@ -221,10 +221,10 @@ class MSEMasterFile(object):
         file_handle.read(3)
         dfi.num_fields = struct.unpack("B", file_handle.read(1))[0]
         file_handle.read(4)
-        dfi.stock_symbol = file_handle.read(14).decode('ascii').strip('\x00')
+        dfi.stock_symbol = file_handle.read(14).rstrip(b'\x00').decode('ascii')
         file_handle.read(7)
         try:
-            dfi.stock_name = file_handle.read(16).decode(self.encoding).strip('\x00')
+            dfi.stock_name = file_handle.read(16).rstrip(b'\x00').decode(self.encoding)
         except Exception, e:
             print("Error while reading the stock name. Did you specify the correct encoding?\n" +
                   "Current encoding: %s, error message: %s" % (self.encoding, e))
